07d742998f9195ffd16ec420a0f7cb9a6f8ee3b9
visokoo <ta.vivian@gmail.com>
2019-04-08 00:08:43 -0700

    removing activity/ from lesson01 and adding dir to .gitignore

--- students/visokoo/lesson01/activity/calculator/adder.py
+++
@@ -1,9 +0,0 @@
-"""Adder class"""
-
-
-class Adder(object):
-    """Adder class"""
-    @staticmethod
-    def calc(operand_1, operand_2):
-        """adding params operand_1 and operand_2"""
-        return operand_1 + operand_2
--- students/visokoo/lesson01/activity/calculator/divider.py
+++
@@ -1,9 +0,0 @@
-"""Dividing class"""
-
-
-class Divider(object):
-    """Dividing class"""
-    @staticmethod
-    def calc(operand_1, operand_2):
-        """dividing params operand_1 and operand_2"""
-        return operand_1/operand_2

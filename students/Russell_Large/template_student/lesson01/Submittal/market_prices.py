90a19428ac1e6dc38e078b87b056adbef660ff25
russlarge256 <russlarge@gmail.com>
2019-04-16 19:59:04 -0700

    Submit lesson01

---
+++ students/Russell_Large/template_student/lesson01/Submittal/market_prices.py
@@ -0,0 +1,8 @@
+'''market prices'''
+def get_latest_price():
+    '''return 24, market price'''
+
+    print("Get price")
+
+    return 24
+    # Raise an exception to force the user to Mock its output

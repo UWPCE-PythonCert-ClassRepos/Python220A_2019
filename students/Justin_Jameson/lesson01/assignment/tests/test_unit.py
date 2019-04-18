d134f52e349f4615816004153ca1ec571c6df84a
Justin Jameson <jjameson@uw.edu>
2019-04-17 18:02:43 -0700

    updating for homework

---
+++ students/Justin_Jameson/lesson01/assignment/tests/test_unit.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from unittest.mock import MagicMock
+import main.py
+
+"""Tests for Main.py:
+1. does each prompt work in 'main_menu'.
+2. when adding content to 'add_new_item', item is added to the dictionary.
+3. 'item_info' either prints the dictionary or 'Item not found in Inventory' 
+
+Tests for inventory_class
+Not a test, but would like to think about, if any of the codes have a specific
+format, such as numbers only.
+1. can take 4 input items and create a dictionary
+
+Tests for furniture_class
+1. can take 6 input items and create a dictionary
+
+Tests for electric_appliances_class
+1. can take 6 input items and create a dictionary
+
+Test for market_prices
+1. can return a value for latest price?
+"""

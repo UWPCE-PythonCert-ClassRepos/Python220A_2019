a171444bef59a0594f0f4ad3f113e6680af1da2a
Justin Jameson <jjameson@uw.edu>
2019-05-02 20:16:48 -0700

    cleaning up directory

---
+++ students/Justin_Jameson/lesson03/assignment/tests/basic_operations_test.py
@@ -0,0 +1,103 @@
+# -------------------------------------------------#
+# # Title: Lesson 03 Test Harness
+# # Dev:   Justin Jameson
+# # Date:  4/20/2019
+# # ChangeLog: (Who, when, What)
+# -------------------------------------------------#
+"""
+Following guidlines set forth in: Testing Python APPLYING UNIT TESTING, TDD, BDD, AND ACCEPTANCE TESTING
+- Unit tests should be placed under a test/unit directory at the top level of your project folder.
+- All folders within the application's code should be mirrored by test folders under test/unit,
+which will have the unit tests for each file in them.
+For example, app/data should have a mirrored folder of test/unit/app/data.
+- All unit test files should mirror the name of the file they are testing, with _testas the suffix.
+For example, app/data/data_interface.py should have a test file of test/unit/app/data/data_interface_test.py.
+"""
+
+import pytest
+import basic_operations as b_ops
+
+
+@pytest.fixture
+def _add_customers():
+    customer_dict = {'customer_id': '6', 'first_name': 'Katee', 'last_name': 'Jane',
+                     'home_address': 'lkasdjf', 'phone_number': '9876',
+                     'email_address': 'none', 'customer_status': 'A',
+                     'credit_limit': '231'}
+    return customer_dict
+
+
+@pytest.fixture
+def _search_customers():
+    customer_id = 6
+    return customer_id
+
+
+@pytest.fixture
+def _delete_customers():
+    customer_id = 6
+    return customer_id
+
+
+@pytest.fixture
+def _update_customer_credit():
+    customer_id = 6
+    new_credit = 369
+    return customer_id, new_credit
+
+
+@pytest.fixture
+def _list_active_customers():
+    return []
+
+
+def test_list_active_customers(_list_active_customers):
+    """ actives """
+
+    actives = b_ops.list_active_customers()
+
+    assert actives == 2
+
+
+def test_add_customer(_add_customers):
+    """ additions
+    """
+
+    b_ops.add_customer(_add_customers)
+    added = b_ops.search_customer(6)
+    assert added["first_name"] == 'Katee'
+    assert added["last_name"] == 'Jane'
+    assert added["email_address"] == 'none'
+    assert added["phone_number"] == 9876
+    b_ops.delete_customer(6)
+
+
+def test_search_customer(_search_customers):
+    """ search """
+    result = b_ops.search_customer(_search_customers)
+    assert result == {'customer_id': '6', 'first_name': 'Katee', 'last_name': 'Jane',
+                      'home_address': 'lkasdjf', 'phone_number': '9876',
+                      'email_address': 'none', 'customer_status': 'A',
+                      'credit_limit': '231'}
+
+    result = b_ops.search_customer(_search_customers)
+    assert result["name"] == 'Katee'
+    assert result["lastname"] == 'Jane'
+
+
+def test_update_customer_credit(_update_customer_credit):
+    """ update """
+    b_ops.update_customer_credit(_update_customer_credit)
+    with pytest.raises(ValueError) as excinfo:
+        b_ops.update_customer_credit("00100", 1000)  # error
+        assert 'NoCustomer' in str(excinfo.value)
+
+
+def test_delete_customer(_delete_customers):
+    """ delete """
+
+    response = b_ops.delete_customer(_delete_customers)
+    assert response is True
+
+    deleted = b_ops.search_customer(_delete_customers)
+    assert deleted == {}

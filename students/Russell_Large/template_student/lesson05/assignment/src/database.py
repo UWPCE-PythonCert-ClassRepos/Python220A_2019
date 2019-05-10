b86e8edfd506eb88d5adaab28cd63edfa94d599e
russlarge256 <45131808+russlarge256@users.noreply.github.com>
2019-05-10 15:10:00 -0700

    Update database.py

--- students/Russell_Large/template_student/lesson05/assignment/src/database.py
+++ students/Russell_Large/template_student/lesson05/assignment/src/database.py
@@ -4,12 +4,13 @@ import pprint
 import os
 from pymongo import MongoClient
 
-db_folder = os.getcwd()
-print(db_folder)
-loc = str(db_folder[:-4] + '\data')
-product = f'{loc}\product.csv'
-customer = f'{loc}\customers.csv'
-rental = f'{loc}\\rental.csv'
+# # add into import
+# db_folder = os.getcwd()
+# # print(db_folder)
+# loc = str(db_folder[:-4] + '\data')
+# product = f'{loc}\product.csv'
+# customer = f'{loc}\customers.csv'
+# rental = f'{loc}\\rental.csv'
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -34,95 +35,128 @@ class MongoDBConnection():
 
 
 def import_data(dir, products, customers, rentals):
+    '''
+    Import different types of CSV data into MongoDB.
+    :param dir: 
+    :param products: 
+    :param customers: 
+    :param rentals: 
+    :return: 
+    '''
     mongo = MongoDBConnection()
 
     with mongo:
         csvlist = [products, customers, rentals]
-
+        product_dict_list = []
+        cust_dict_list = []
+        rentals_dict_list = []
+        errors = []
         for file in csvlist:
             if file == products:
-
+                errors_count = 0
                 # append csv to dict list
                 reader = csv.reader(open(products, 'r'))
-                product_dict_list = []
-                for row in reader:
-                    product_dict = ({
-                            '_id': row[0],
-                            'description': row[1],
-                            'product_type': row[2],
-                            'quantity_available': row[3],
-                            })
-                    product_dict_list.append(product_dict)
 
-                # connect to mongodb, create collection
-                db = mongo.connection.HP_Norton
-                collection = db['Products']
-                collection.drop()
+                try:
+                    for row in reader:
+                        product_dict = ({
+                                '_id': row[0],
+                                'description': row[1],
+                                'product_type': row[2],
+                                'quantity_available': row[3],
+                                })
+                        product_dict_list.append(product_dict)
 
-                # insert data to 'Products' collection
-                result = collection.insert_many(product_dict_list)
+                    # connect to mongodb, create collection
+                    db = mongo.connection.HP_Norton
+                    collection = db['Products']
+                    collection.drop()
 
-                # produce count from input
-                count = len(result.inserted_ids)
-                #logger.info(f'test{pprint.pprint(product_dict_list)}')
-                logger.info(f'Total Product records added: {count}')
-                # print(f'Total records added: {count}')
+                    # insert data to 'Products' collection
+                    result = collection.insert_many(product_dict_list)
+
+                    # produce count from input
+                    count = len(result.inserted_ids)
+                    #logger.info(f'test{pprint.pprint(product_dict_list)}')
+                    logger.info(f'Total Product records added: {count}')
+                    # print(f'Total records added: {count}')
+                except Exception as e:
+                    logger.info('Error occured adding data to db.')
+                    logger.info(e)
+                    errors_count += 1
+                    errors.append('product errors:' + str(errors_count))
+
+            #provide record count of number of items
+            # provide count of any errors that occured, in the same order
 
             elif file == customers:
-
+                errors_count = 0
                 # append csv to dict list
                 reader = csv.reader(open(customers, 'r'))
-                cust_dict_list = []
-                for row in reader:
-                    cust_dict = ({'_id': row[0],
-                             'cust_name': row[1],
-                             'cust_address': row[2],
-                             'cust_zip': row[3],
-                             'cust_phone': row[4],
-                             'cust_email': row[5]
-                             })
-                    cust_dict_list.append(cust_dict)
+                try:
+                    for row in reader:
+                        cust_dict = ({'_id': row[0],
+                                 'cust_name': row[1],
+                                 'cust_address': row[2],
+                                 'cust_zip': row[3],
+                                 'cust_phone': row[4],
+                                 'cust_email': row[5]
+                                 })
+                        cust_dict_list.append(cust_dict)
 
-                # connect to mongodb, create collection
-                db = mongo.connection.HP_Norton
-                collection = db['Customer']
-                collection.drop()
+                    # connect to mongodb, create collection
+                    db = mongo.connection.HP_Norton
+                    collection = db['Customer']
+                    collection.drop()
 
-                # insert data to 'Products' collection
-                result = collection.insert_many(cust_dict_list)
+                    # insert data to 'Products' collection
+                    result = collection.insert_many(cust_dict_list)
 
-                # produce count from input
-                count = len(result.inserted_ids)
-                #logger.info(f'{pprint.pprint(cust_dict_list)}')
-                logger.info(f'Total Customer records added: {count}')
-                # print(f'Total records added: {count}')
+                    # produce count from input
+                    count = len(result.inserted_ids)
+                    #logger.info(f'{pprint.pprint(cust_dict_list)}')
+                    logger.info(f'Total Customer records added: {count}')
+                    # print(f'Total records added: {count}')
+                except Exception as e:
+                    logger.info('Error occured adding data to db.')
+                    logger.info(e)
+                    errors_count += 1
+                    errors.append('customer errors:' + str(errors_count))
 
 
             elif file == rentals:
-
-                # append csv to dict list
+                errors_count = 0
                 reader = csv.reader(open(rentals, 'r'))
-                rentals_dict_list = []
-                for row in reader:
-                    rentals_dict = ({'product_id': row[0],
-                                    'user_id': row[1]
-                                    })
-                    rentals_dict_list.append(rentals_dict)
+                try:
+                    # append csv to dict list
+                    for row in reader:
+                        rentals_dict = ({'product_id': row[0],
+                                        'user_id': row[1]
+                                        })
+                        rentals_dict_list.append(rentals_dict)
 
-                # connect to mongodb, create collection
-                db = mongo.connection.HP_Norton
-                collection = db['Rentals']
-                collection.drop()
+                    # connect to mongodb, create collection
+                    db = mongo.connection.HP_Norton
+                    collection = db['Rentals']
+                    collection.drop()
 
-                # insert data to 'Products' collection
-                result = collection.insert_many(rentals_dict_list)
+                    # insert data to 'Products' collection
+                    result = collection.insert_many(rentals_dict_list)
 
-                # produce count from input
-                count = len(result.inserted_ids)
-                #logger.info(f'{pprint.pprint(rentals_dict_list)}')
-                logger.info(f'Total Rentals records added: {count}')
-                # print(f'Total records added: {count}')
+                    # produce count from input
+                    count = len(result.inserted_ids)
+                    #logger.info(f'{pprint.pprint(rentals_dict_list)}')
+                    logger.info(f'Total Rentals records added: {count}')
+                    # print(f'Total records added: {count}')
 
+                except Exception as e:
+                    logger.info('Error occured adding data to db.')
+                    logger.info(e)
+                    errors_count += 1
+                    errors.append('customer errors:' + str(errors_count))
+
+        all_results = (product_dict_list, cust_dict_list, rentals_dict_list)
+        return tuple(all_results), tuple(errors)
 
 def show_available_products():
     mongo = MongoDBConnection()
@@ -138,17 +172,21 @@ def show_available_products():
             products = avail['description']
             results_dict[id] = avail
             logger.info(f'Products available: {products}')
-        return results_dict
+
+        test = tuple(results_dict.keys())
+
+        return results_dict, test
 
 def show_rentals(product_id):
     mongo = MongoDBConnection()
+    results = {}
 
     with mongo:
         db = mongo.connection.HP_Norton
         rentals = db['Rentals']
 
         try:
-            results = {}
+
             for info in rentals.aggregate([
                 {
                     '$lookup':
@@ -161,24 +199,11 @@ def show_rentals(product_id):
                 },
                             {'$match': {'product_id': product_id}}]):
                 results['Rentals'] = info
-                print(results)
-                # pprint.pprint(info)
-
-            return results
 
         except Exception as e:
             print(e)
             logger.info(f'show rentals info not successful. {e}')
 
+    test = f"{results.keys()}"
 
-
-
-
-
-
-
-
-
-# import_data('data', product, customer, rental)
-# show_available_products()
-# show_rentals('prd001')
+    return results, test

4885bdd18d1f864d0e6e12b9f3772a9d792c8a12
russlarge256 <45131808+russlarge256@users.noreply.github.com>
2019-05-15 15:04:46 -0700

    Add files via upload

---
+++ students/Russell_Large/template_student/lesson06/assignment/src/create_large_dataset.py
@@ -0,0 +1,89 @@
+import string
+import random
+import time
+import csv
+import sys
+import pandas as pd
+import numpy as np
+
+sys.path.append(r'N:\Python220\lesson06\Lesson06\assignment\data')
+
+newcsv = r'N:\Python220\lesson06\Lesson06\assignment\data\test.csv'
+
+def get_random_date():
+
+    # 07/21/1901
+
+    month = random.randrange(1, 12)
+    day = random.randrange(1, 30)
+    year = random.randrange(1900, 2019)
+
+    return f'{month}/{day}/{year}'
+
+
+def ccnum_gen():
+
+    number = random.randrange(1000000000000000, 9999999999999999)
+
+    return number
+
+def guid_section_gen(size, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for n in np.array(range(size)))
+
+def guid_full_gen():
+
+    first = guid_section_gen(8)
+    second = guid_section_gen(4)
+    third = guid_section_gen(4)
+    fourth = guid_section_gen(4)
+    fifth = guid_section_gen(8)
+
+    package = f'{first}-{second}-{third}-{fourth}-{fifth}'
+
+    return package
+
+def char_gen(chars=string.ascii_lowercase):
+
+    size = random.randrange(2, 10)
+
+    t = ''.join(random.choice(chars) for n in np.array(range(size)))
+
+    return t
+
+def sentence():
+    return ' '.join(char_gen() for x in range(1,13))
+
+
+#### Map ####
+
+beginning_time = time.time()
+tot_list2 = []
+count = 1
+for i in np.array(range(1, 1000000)):
+
+    # seq, guid, seq, seq, ccnumber, date, sentence
+
+    generated_data = count, guid_full_gen(), count, count, ccnum_gen(), get_random_date(), sentence()
+    count += 1
+    tot_list2.append(generated_data)
+elapsed_time = time.time() - beginning_time
+
+
+df = pd.DataFrame(tot_list2)
+df.to_csv(newcsv, index=False)
+
+# def create_dataset():
+#     tot_list2 = []
+#     count = 1
+#     for i in map(np.array, range(1, 1000000)):
+#         # seq, guid, seq, seq, ccnumber, date, sentence
+#
+#         generated_data = count, guid_full_gen(), count, count, ccnum_gen(), get_random_date(), sentence()
+#         count += 1
+#         tot_list2.append(generated_data)
+#     elapsed_time = time.time() - beginning_time
+#
+#     df = pd.DataFrame(tot_list2)
+#     df.to_csv(newcsv, index=False)
+#
+#     return

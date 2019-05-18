456bd658a014bba9583a40e6b623d06c91e6020b
Justin Jameson <jjameson@uw.edu>
2019-05-18 13:48:23 -0700

    created a class for Mongo info then called lists of dictionaries from the class.

--- students/Justin_Jameson/lesson05/assignment/src/database.py
+++ students/Justin_Jameson/lesson05/assignment/src/database.py
@@ -1,13 +1,20 @@
+from pymongo import MongoClient
+import sys
+import csv
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# ************************************** Defining variables *************************************
 directory_name = '..\\.\\data\\'
 product_data = 'product.csv'
 customer_data = 'customers.csv'
 rentals_data = 'rental.csv'
-data_files = product_data, customer_data, rentals_data,
+logger.info('defined directory name and the following .csv files: product_data, customer_data, and rental_data')
 
 """
-    As a HP Norton customer I want to see a list of all products available for rent so that I can make a rental choice.
-    You can have a specific field to indicate if a product is available, however,
-    a quantity_available of 0 is understood as “not available”.
+   
 
     As a HP Norton salesperson I want to see:
      1. a list of all of the different products, showing product ID, description, product type and quantity available.
@@ -21,49 +28,230 @@ Here is what you need to do:
     3. Write queries to retrieve the product data.
     4. Write a query to integrate customer and product data (join data).
 
-Other requirements:
+Other requirements:"""
 
-    Your code should not trigger any warnings or errors from Pylint.
+# ******************************************** Defining Classes *********************************
+class MongoDBConnection:
+    """
+    MongoDB Connection
+    """
 
-Testing
+    def __init__(self, host='127.0.0.1', port=27017):
+        """ be sure to use the ip address not name for local windows"""
+        self.host = host
+        self.port = port
+        self.connection = None
+        logger.info('created connection to host {} and port {}'.format(self.host, self.port))
 
-In order for your code to be evaluated, you need to create a file called database.py with the following functions:
+    def __enter__(self):
+        self.connection = MongoClient(self.host, self.port)
+        return self
 
-    def import_data(directory_name, product_file, customer_file, rentals_file):
-        This function takes a directory name and three csv files as input,then creates and populates a new MongoDB
-        database.
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.connection.close()
+
+
+# ************************************* user interface *************************************
+def main_menu(user_prompt=None):
+    """
+    This method creates the menu for the program.
+    """
+    valid_prompts = {"1": show_available_products,
+                     "2": show_rentals,
+                     "3": read_csv_files,
+                     "4": read_csv_files,
+                     "5": reset_db,
+                     "q": exit_program}
+    options = list(valid_prompts.keys())
+
+    while user_prompt not in valid_prompts:
+        options_str = ("{}" + ", {}" * (len(options)-1)).format(*options)
+        print(f"Please choose from the following options ({options_str}):")
+        print("1. Show Customer products available for rent")
+        print("2. Show Sales Person the list of rentals")
+        print("3. Load csv files to database.")
+        print("4. Update the Customer's credit line")
+        print("5. Reset the DataBase")
+        print("q. Quit")
+        user_prompt = input(">")
+    return valid_prompts.get(user_prompt)
+
+
+# ************************************* Processing *****************************************
+def import_data(directory_location, product_file, customer_file, rental_file):
+    """
+    This function takes a directory name and three csv files as input,
+    then creates a directory and file name for use in 'ReadCsvFiles' method.
            1. product data = product.csv
            2. customer data = customers.csv
            3. rentals data = rental.csv
-        Return: 2 tuples:
-        1. a record count of the number of products, customers and rentals added (in that order)
-        2. a count of any errors that occurred, in the same order."""
-def import_data(directory_name, product_file, customer_file, rental_file):
-
+    :param directory_location:
+    :param product_file:
+    :param customer_file:
+    :param rental_file:
+    :return:import_customer, import_product, import_rentals
     """
-    def show_available_products():
-        Returns a Python dictionary of products listed as available with the following fields:
-            product_id.
-            description.
-            product_type.
-            quantity_available.
+    import_customer = directory_location + customer_file
+    import_product = directory_location + product_file
+    import_rentals = directory_location + rental_file
+    logger.info('returning tuple for use in method "ReadCsvFiles"')
+    return import_customer, import_product, import_rentals
 
-         For example:
 
-         {‘prd001’:{‘description’:‘60-inch TV stand’,’product_type’:’livingroom’,’quantity_available’:‘3’},’prd002’:
-         {‘description’:’L-shaped sofa’,’product_type’:’livingroom’,’quantity_available’:‘1’}}
+def read_csv_files():
+    """
+    This method calls 'import_data' method to access the csv files.
+    Then reads the csv files, and turns them into a list of dictionaries.
+    Then sends the list to the 'main' method for incorporation into MongoDB.
 
-    def show_rentals(product_id):
-        Returns a Python dictionary with the following user information from users that have rented products matching product_id:
-            user_id.
-            name.
-            address.
-            phone_number.
-            email.
+    :return:
+    """
+    imported_data = import_data(directory_name, product_data, customer_data, rentals_data)
+    customers_file = imported_data[0]
+    products_file = imported_data[1]
+    rentals_file = imported_data[2]
+    list_of_customers = []
+    list_of_products = []
+    list_of_rentals = []
+    with open(customers_file, 'r') as customer:
+        reader = csv.reader(customer)
+        for row in reader:
+            c_dict = {'User ID': row[0], 'Name': row[1], 'Address': row[2],
+                      'zip code': row[3], 'phone number': row[4], 'email': row[5]}
+            list_of_customers.append(c_dict)
+        list_of_customers.pop(0)
+    with open( products_file, 'r') as product:
+        reader = csv.reader(product)
+        for row in reader:
+            p_dict = {'Product ID': row[0], 'Description': row[1], 'Product Type': row[2], 'Quantity available': row[3]}
+            list_of_products.append(p_dict)
+        list_of_products.pop(0)
+    with open(rentals_file, 'r') as rentals:
+        reader = csv.reader(rentals)
+        for row in reader:
+            r_dict = {'Product ID': row[0], 'User ID': row[1]}
+            list_of_rentals.append(r_dict)
+        list_of_rentals.pop(0)
+    main(list_of_customers, list_of_products, list_of_rentals)
 
-         For example:
 
-         {‘user001’:{‘name’:’Elisa Miles’,’address’:‘4490 Union Street’,’phone_number’:‘206-922-0882’,’email’:’elisa.miles@yahoo.com’},
-         ’user002’:{‘name’:’Maya Data’,’address’:‘4936 Elliot Avenue’,’phone_number’:‘206-777-1927’,’email’:’mdata@uw.edu’}}
+def main(list_of_customers, list_of_products, list_of_rentals):
+    """
+     Return: 2 tuples:
+            1. a record count of the number of products, customers and rentals
+            added (in that order)
+            2. a count of any errors that occurred, in the same order.
+    :param list_of_customers:
+    :param list_of_products:
+    :param list_of_rentals:
+    :return:
+    """
+    mongo = MongoDBConnection()
 
+    with mongo:  # Context manager.
+        # mongodb database; create rentaldatabase
+        db = mongo.connection.rentaldatabase
+
+        # collection (table) in database, (3) customers, products and rental status (rental)
+        customers = db["customers"]
+        products = db["products"]
+        rental = db["rental"]
+        result_customer = customers.insert_many(list_of_customers)  # Inserts records specified in the list above
+        result_products = products.insert_many(list_of_products)
+        result_rentals = rental.insert_many(list_of_rentals)
+        # print_mdb_collection(customers)  # Prints all records from a specified table
+        # print_mdb_collection(products)
+        # print_mdb_collection(rental)
+        return result_customer, result_products, result_rentals
+
+""" # related data
+for CurrentRecord in ThePersonWhoCollects.find():  # Here CurrentRecord is a variable 
+pointing to each found record in a table
+    # CurrentRecord is a dictionary (EACH record is a dictionary). We can refer an element of the dictionary
+    # by its key; one of the keys is called "name"
+    print(customer'List for {CurrentRecord["name"]}')
+    query = {"name": CurrentRecord["name"]}
+    for a_cd in cd.find(
+            query):  # Find all the records from table "cd" where attribute name 
+            is equal to a specified value (which happens to be a
+        # a name of a current record). In general, keys from records need satisfy properties specified in query
+        print(customer'{CurrentRecord["name"]} has collected {a_cd}')
 """
+
+
+def reset_db():
+    """
+    deletes db
+    :return:
+    """
+    mongo = MongoDBConnection()
+
+    with mongo:  # Context manager.
+        db = mongo.connection.rentaldatabase
+    # start afresh next time?
+    drop_data = input("Drop data?")
+    if drop_data.upper() == 'Y':
+        db['customers'].drop()
+        db["products"].drop()
+        db["rental"].drop()
+
+
+# ****************************************** output to the user *************************************
+def print_mdb_collection(collection_name):
+    """
+    This method is called from main to print products, customers, and rentals.
+    :param collection_name:
+    :return:
+    """
+    for doc in collection_name.find():
+        logger.info('producing a list of products, customers, and rentals.')
+        print(doc)
+
+
+def show_available_products():
+    """
+    As a HP Norton customer I want to see a list of all products available for rent so that I can make a rental choice.
+       You can have a specific field to indicate if a product is available, however,
+       a quantity_available of 0 is understood as “not available”.
+       Returns a Python dictionary of products listed as available with the following fields:
+           product_id.
+           description.
+           product_type.
+           quantity_available.
+
+    For example:
+        {‘prd001’:{‘description’:‘60-inch TV stand’,’product_type’:’livingroom’,’quantity_available’:‘3’},’prd002’:
+        {‘description’:’L-shaped sofa’,’product_type’:’livingroom’,’quantity_available’:‘1’}}
+    :return:
+    """
+
+    for doc in main():
+
+        logger.info('producing a list of products, customers, and rentals.')
+        print(doc)
+
+def show_rentals(product_id):
+    """
+    Returns a Python dictionary with the following user information from users that have rented products
+    matching product_id:
+        user_id.
+        name.
+        address.
+        phone_number.
+        email.
+     For example:
+     {‘user001’:{‘name’:’Elisa Miles’,’address’:‘4490 Union Street’,’phone_number’:‘206-922-0882’,’email’:’elisa.miles@yahoo.com’},
+     ’user002’:{‘name’:’Maya Data’,’address’:‘4936 Elliot Avenue’,’phone_number’:‘206-777-1927’,’email’:’mdata@uw.edu’}}
+    """
+    pass
+
+
+# ***************************************** execution ***********************************************
+def exit_program():
+    """This method exits the program"""
+    logger.info('called exit_program')
+    sys.exit()
+
+
+if __name__ == "__main__":
+    main_menu()()
